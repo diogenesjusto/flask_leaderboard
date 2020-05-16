d88fe48bd8453da26834690c2bca3ae68b886006
Diogenes Justo <diogenes.justo@gmail.com>
2020-05-16 00:28:08 -0300

    Update scorer.py

--- scorer.py
+++ scorer.py
@@ -1,10 +1,12 @@
 import pandas as pd 
 
+from sklearn.metrics import f1_score
+from sklearn import preprocessing
 from sklearn.metrics import mean_squared_error
 
 class Scorer():
     def __init__(self, public_path = './master_key/public_key.csv', 
-                private_path = './master_key/private_key.csv', metric = mean_squared_error):
+                private_path = './master_key/private_key2.csv', metric = mean_squared_error):
         self.public_path = public_path
         self.private_path = private_path
         self.metric = metric
@@ -32,12 +34,29 @@ class Scorer():
         if len(df_key) != len(df_merged):
             return ("NOT SAME INDEX", None)
 
+        sub_true = df_merged['SUB-CATEGORIA']
+        cat_true = df_merged['CATEGORIA']
+        
         # data size is same, time to get score
-        y_key = df_merged['prediction_key']
-        y_submission = df_merged['prediction_submission']
+        sub_key = df_merged['SUB-CATEGORIA_key']
+        sub_submission = df_merged['SUB-CATEGORIA_submission']
 
-        if y_submission.isna().sum() > 0:
+        cat_key = df_merged['CATEGORIA_key']
+        cat_submission = df_merged['CATEGORIA_submission']
+
+        if sub_submission.isna().sum() > 0 or cat_submission.isna().sum() > 0:
             return ("SUBMISSION HAS NULL VALUE", None)
 
-        score = self.metric(y_key, y_submission)
+        encoder = preprocessing.LabelEncoder()
+        
+        sub_key = encoder.fit_transform(sub_key)
+        cat_key = encoder.fit_transform(cat_key)
+        sub_submission = encoder.fit_transform(sub_submission)
+        cat_submission = encoder.fit_transform(cat_submission)
+        
+        f1_cat = f1_score(cat_key, cat_submission, average='micro')
+        f1_sub = f1_score(sub_key, sub_submission, average='micro')
+        final_score = (f1_score(cat_true, cat_pred, average='micro')+f1_score(sub_true, sub_pred, average='micro'))/2
+        
+        score = final_score
         return ("SUBMISSION SUCCESS", score)
