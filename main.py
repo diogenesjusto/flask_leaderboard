783825cec7f15f84eaca35c6995e1fbd66459a14
Diogenes Justo <diogenes.justo@gmail.com>
2020-05-16 00:48:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ from scorer import Scorer
 limit_lb = 100 # Number of user showed at leaderboard table
 greater_better = False # True if lowest score is the best; False if greatest score is the best
 metric = mean_absolute_error #change the metric using sklearn function
-scorer = Scorer(public_path = './master_key/public_key.csv', 
-                private_path = './master_key/private_key.csv', 
+scorer = Scorer(public_path = './master_key/public_key2.csv', 
+                private_path = './master_key/private_key2.csv', 
                 metric = metric) #change the metric using sklearn function
 
 ## Upload parameter
