858844ae1bcd685c57702b145ab10e897e5601bb
Diogenes Justo <diogenes.justo@gmail.com>
2020-05-16 00:13:29 -0300

    Update scorer_2.py

--- scorer_2.py
+++ scorer_2.py
@@ -4,9 +4,9 @@ from sklearn.metrics import f1_score
 from sklearn import preprocessing
 from sklearn.metrics import mean_squared_error
 
-class Scorer():
+class Scorer2():
     def __init__(self, public_path = './master_key/public_key.csv', 
-                private_path = './master_key/private_key.csv', metric = mean_squared_error):
+                private_path = './master_key/private_key2.csv', metric = mean_squared_error):
         self.public_path = public_path
         self.private_path = private_path
         self.metric = metric
