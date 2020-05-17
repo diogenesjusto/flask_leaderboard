7c949de1cc84e73115b4d629b246b37e95fa4086
Diogenes Justo <diogenes.justo@gmail.com>
2020-05-17 01:58:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -55,8 +55,9 @@ class User(UserMixin, db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    password = db.Column(db.String(128)) ## Too lazy to make it hash
-
+    password = db.Column(db.String(128),
+    email = db.Column(db.String(128)) ## Too lazy to make it hash
+    
     def __repr__(self):
         return self.username
 
