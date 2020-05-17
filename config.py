e31c23741585fb272de76b261d40f93f798dd02c
Diogenes Justo <diogenes.justo@gmail.com>
2020-05-17 01:02:16 -0300

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ print(basedir)
 class Config(object):
     # ...
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'sqlite:///' + os.path.join(basedir, '/s3/app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
