599b28c5993f8348cf53647227e33053fcc38425
Diogenes Justo <diogenes.justo@gmail.com>
2020-05-22 00:15:08 -0300

    Update scorer.py

--- scorer.py
+++ scorer.py
@@ -68,5 +68,5 @@ class Scorer():
         final_score = ((f1_cat)+(f1_sub))
         final_score = final_score
         
-        score = '%.5f' % ( sum(np.where(cat_key == cat_submission, 1, 0)) )
+        score = ( sum(np.where(cat_key == cat_submission, 1, 0)) )
         return ("SUBMISSION SUCCESS", score)
